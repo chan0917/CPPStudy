00dc48eac55ebd0918f0d51c7098d91dd2d3f52d
chan0917 <chan34321@gmail.com>
2023-05-09 16:30:21 +0900

    Learning Struct

---
+++ Structer/Structer/Structer.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+typedef struct score
+{
+    char name[20];
+    int E_score;
+}S;
+
+int main()
+{
+    S s1 = { "EAE", 82 };
+
+    std::cout << s1.E_score << s1.name << std::endl;
+}
