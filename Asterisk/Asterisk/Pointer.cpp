0e9a4dd7a93e069c6702e8d220e3e232a4b0b5b5
chan0917 <chan34321@gmail.com>
2023-05-09 23:41:23 +0900

    Learning Stack and Heap
    stack heap sack heap nvm

--- Asterisk/Asterisk/Pointer.cpp
+++ Asterisk/Asterisk/Pointer.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
+int data = 10; //전역변수와 static은 데이터 영역에 할당
+
 int main()
 {
-	int a = 1;
+	int a = 1; // 지역변수, 매개변수 스택에 저장 
 	int* b = &a; //b에 a의 주소값을 저장
 	
 	cout << b << endl;
