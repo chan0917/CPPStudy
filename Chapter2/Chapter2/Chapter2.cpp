77adaaa4685e6043b613dd51de4da5c87a17a706
chan0917 <chan34321@gmail.com>
2023-05-13 23:55:49 +0900

    Start Chapter2
    EAE

---
+++ Chapter2/Chapter2/Chapter2.cpp
@@ -0,0 +1,17 @@
+﻿// Chapter2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#define TRUE 1
+#define FALSE 0
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    cout << "true: " << true << endl;
+
+}
+
+
