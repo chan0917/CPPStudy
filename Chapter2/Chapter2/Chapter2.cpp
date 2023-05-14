f74e31361ae206576d3da3a40496409f7ad5d209
chan0917 <chan34321@gmail.com>
2023-05-14 21:37:13 +0900

    Learning Reference(1)

--- Chapter2/Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2/Chapter2.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #define TRUE 1
-#define FALSE 0
+#define FALSE 0 //true와 false는 1과 0이 아니다. 그저 '참'과 '거짓'을 표현하기위한 1바이트 크기의 데이터일 뿐이다 
 using namespace std;
 
 int main()
@@ -11,6 +11,13 @@ int main()
     std::cout << "Hello World!\n";
 
     cout << "true: " << true << endl;
+
+    //참조자 시작
+    int num1 = 2000;
+    int& num2 = num1;
+    num2 = 1000;
+    cout << &num2 << endl;
+    cout << num1 << endl; //참조자는 변수에 별명을 하나 붙이는 것이다 ex) "예찬아 늦었어" == "하야시 늦었어"
 
 }
 
