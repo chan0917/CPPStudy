da0b7455d86054a0e2fcb487f8bad5bcd473750c
chan0917 <chan34321@gmail.com>
2023-05-24 23:51:55 +0900

    Learning -> vs .
    is they have difference?

---
+++ AccessVS/AccessVS/AccessVS.cpp
@@ -0,0 +1,26 @@
+﻿// AccessVS.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Johnson { // 클래스
+    public:       // 접근제한자
+        int age;    // 속성 변수
+        string name;// 속성 변수
+};
+
+int main()
+{
+    Johnson jjs;
+
+    Johnson* js = new Johnson;
+    jjs.age = 10; // .은 클래스의 맴버를 직접 접근 
+    js -> name = "Johnson"; // -> 포인터를 이용해서 접근  x -> y == (x*).y
+
+    cout << js -> age << endl;
+    cout << js -> name;
+}
+
+
