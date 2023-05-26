4c847a8a8d541321c3cd647d547d6bffacee1e98
chan0917 <chan34321@gmail.com>
2023-05-27 01:37:45 +0900

    DisarraryCalculator
    make AO's addon

---
+++ DisarraryCalculator/DisarraryCalculator.cpp
@@ -0,0 +1,21 @@
+﻿// DisarraryCalculator.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float A;
+    float B;
+    double E;
+    cout << "우리 혼란: " ;
+    cin >> A;
+    cout << "상대 혼란: " ;
+    cin >> B;
+
+    E = (1 - A / 100) / (1 - B / 100);
+    cout << "약해짐: " << E;
+}
+
+
