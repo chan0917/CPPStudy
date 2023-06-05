8734043862ed5d897f52e4dede155c736debe25d
chan0917 <chan34321@gmail.com>
2023-06-05 23:54:51 +0900

    Learning OOP(FINAL)
    bye Chapter3

--- chapter3/chapter3/OOP.cpp
+++ chapter3/chapter3/OOP.cpp
@@ -49,3 +49,9 @@ void FruitBuyer::ShowBuyResult()
 //	buyer.InitMembers(5000);
 //	buyer.BuyApples(seller, 2000);
 //}
+
+/*위 예제를 보면 객체에서 다른 객체에 존재하는 함수를 호출하는 것을 볼 수 있다
+* 
+* 하나의 객체가 다른 하나의 객체에게 메세지를 전달하는 방법은 함수호출을 기반으로 한다.
+* 그래서 이러한 형태의 함수호출을 가리켜 '메세지 전달(Message Passing)' 이라고 한다
+*/
